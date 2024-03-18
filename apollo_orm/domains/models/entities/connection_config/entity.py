3647237314d71e7b4d5a75204b35356136314e60
Danilo Rodrigues <daniloarodrigues@outlook.com>
2024-03-18 18:59:43 -0300

    Table validate in CocnnectionConfig

--- apollo_orm/domains/models/entities/connection_config/entity.py
+++ apollo_orm/domains/models/entities/connection_config/entity.py
@@ -5,7 +5,7 @@ from apollo_orm.domains.models.entities.credentials.entity import Credentials
 
 class ConnectionConfig:
     def __init__(self, credentials: Credentials, tables: List[str]):
-        if len(tables) == 0:
+        if not tables:
             raise ValueError("Tables cannot be empty")
         self.credential: Credentials = credentials
         self.tables: List[str] = tables
